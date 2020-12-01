51089772c8b5de6fec76ea87d367e1686c9627c2
koran23 <ahdari33@gmail.com>
2020-12-01 16:36:12 -0700

    register

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const db = require('../db/models');
+
+const { csrfProtection, asyncHandler } = require('../utils');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/users/register', csrfProtection, asyncHandler(async(req, res) => {
+  const user = db.User.build();
+  res.render('sign-up', {
+    user,
+    csrfToken: req.csrfToken()
+  });
+}));
+
+
 
 module.exports = router;
