81402d10415dc1d4f27a67bdb81a71adebbbfa29
Samuel <samuelnunn90@gmail.com>
2020-12-01 18:46:18 -0500

    update Auth

--- auth.js
+++ auth.js
@@ -1,52 +1,36 @@
-const jwt = require('jsonwebtoken');
-const { jwtConfig } = require('./config');
-const { User } = require('./db/models');
-const bearerToken = require('express-bearer-token');
+const db = require('./db/models');
 
-const { secret, expiresIn } = jwtConfig;
-
-const getToken = (user) => {
-    const tokenData = {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        watchList: user.watchListId,
-    };
-
-    const token = jwt.sign(
-        { data: tokenData},
-        secret,
-        { expiresIn: parseInt(expiresIn, 10)}
-    );
-    return token;
+const loginUser = (req, res, user) => {
+  req.session.auth = {
+    userId: user.id,
+  };
 };
 
-const createUser = (req, res, next) => {
-    const { token } = req;
-    if(!token) {
-        return res.set('WWW-Authenticate', 'Bearer').status(401).end();
+const restoreUser = async (req, res, next) => {
+  console.log(req.session);
+
+  if (req.session.auth) {
+    const { userId } = req.session.auth;
+
+    try {
+      const user = await db.User.findByPk(userId);
+
+      if (user) {
+        res.locals.authenticated = true;
+        res.locals.user = user;
+        next();
+      }
+    } catch (err) {
+      res.locals.authenticated = false;
+      next(err);
     }
-    return jwt.verify(token, secret, null, async (err, jwtPayLoad) => {
-        if (err) {
-            err.status = 401;
-            return next(err);
-        }
-
-        const { id } = jwtPayLoad.data;
-
-        try {
-            req.user = await User.findByPk(id);
-        } catch (e) {
-            return next(e);
-        }
-
-        if (!req.user) {
-            return res.set('WWW-Authenticate', 'Bearer').status(401).end();
-        }
-        return next();
-    });
+  } else {
+    res.locals.authenticated = false;
+    next();
+  }
 };
 
-const requireAuth = [bearerToken({ cookie: { signed: true, secret: '???', key: '???' } }), createUser];
-
-module.exports = { getToken, requireAuth };
+module.exports = {
+  loginUser,
+  restoreUser,
+};
