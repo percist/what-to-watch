dca28cbfe7904df5c1c5f5a1eef8aee82aa40dad
koran23 <ahdari33@gmail.com>
2020-12-02 14:05:42 -0700

    logout

--- auth.js
+++ auth.js
@@ -6,6 +6,18 @@ const loginUser = (req, res, user) => {
   };
 };
 
+const logoutUser = (req, res) => {
+  delete req.session.auth;
+};
+
+
+const requireAuth = (req, res, next) => {
+  if (!res.locals.authenticated) {
+    return res.redirect('/user/login');
+  }
+  return next();
+}
+
 const restoreUser = async (req, res, next) => {
   console.log(req.session);
 
